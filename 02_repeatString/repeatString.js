b62d4579a6c3933147124b665338a807c221c3ab
Bryan Medina <bryan.medina.per@gmail.com>
2022-06-27 15:25:55 -0500

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+  let res = '';
+  if(times < 0){
+    return 'ERROR';
+  }
+  else{
+    for(let i = 0; i < times; i++){
+      res+=str;
+    }
+    return res;
+  }
+  
 };
 
 // Do not edit below this line

8b743d813fd444ec2fca4424f849cd4c71b630c6
Bryan Medina <bryan.medina.per@gmail.com>
2022-06-28 12:04:49 -0500

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+  toRemove.forEach(num => {
+    let x = arr.indexOf(num);
+    if((x !== -1)){
+      arr.splice(x, 1);
+    }
+  })
+  return arr;
 };
 
 // Do not edit below this line
